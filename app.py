f890d3897efa83b9a17f5c0e7a6209891e93a910
omjadas <omja.das@gmail.com>
2018-03-31 11:55:56 +1100

    implemented device_code function

--- app.py
+++ app.py
@@ -1,20 +1,44 @@
-import config
 from urllib.request import Request, urlopen
 import json
 
-values = {
-        "client_id": config.trakt["client_id"]
+
+def read_config():
+    config_file = open("config.json")
+    config_str = config_file.read()
+    config_file.close()
+    config_data = json.loads(config_str)
+    return config_data
+
+
+def device_code(config_data):
+    values = {
+        "client_id": config_data["trakt"]["client_id"]
     }
 
-data = json.dumps(values).encode("utf-8")
+    post_data = json.dumps(values).encode("utf-8")
 
-headers = {
-    'Content-Type': 'application/json'
-}
-request = Request(
-    'https://private-anon-e286eacc07-trakt.apiary-mock.com/oauth/device/code',
-    data=data,
-    headers=headers)
+    headers = {
+        'Content-Type': 'application/json'
+    }
 
-response_body = urlopen(request).read()
-print(response_body)
+    request = Request(
+        'https://private-anon-e286eacc07-trakt.apiary-mock.com/oauth/device/code',
+        data=post_data,
+        headers=headers)
+
+    response_body = urlopen(request).read()
+
+    response_data = json.loads(response_body)
+
+    print(
+        "Enter {} at {}".format(
+            response_data["user_code"],
+            response_data["verification_url"]))
+
+    return response_data
+
+
+def main():
+    config_data = read_config()
+    device_code(config_data)
+
